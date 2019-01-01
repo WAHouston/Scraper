cd313997219d6d26eaeb32c43446ba75a909494a
Will H <wandrewhouston@gmail.com>
2019-01-01 17:45:41 -0500

    added models

--- server.js
+++ server.js
@@ -14,12 +14,61 @@ app.get("/scrape", function(req, res) {
     axios.get("https://www.coolstuffinc.com/").then(function(response) {
         var $ = cheerio.load(response.data)
         $("div.gm-article-preview-header").each(function(i, element) {
-            var title = $(element).find("h1").text()
-            var link = $(element).find("a").attr("href")
-            var summary = $(element).find("div.gm-article-preview-excerpt").text()
+            var result = {}
+            result.title = $(this)
+                .find("h1")
+                .text()
+            result.link = $(this)
+                .find("a")
+                .attr("href")
+            result.summary = $(this)
+                .find("div.gm-article-preview-excerpt")
+                .text()
             console.log(title)
             console.log(link)
             console.log(summary)
+            db.Article.create(result)
+                .then(function(dbArticle){
+                    console.log(dbArticle)
+                })
+                .catch(function(err) {
+                    console.log(err)
+                })
         })
+        res.send("Scrape Complete")
     })
+})
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle)
+        })
+        .catch(function(err) {
+            res.json(err)
+        })
+})
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle)
+        })
+        .catch(function(err) {
+            res.json(err)
+        })
+})
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true })
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle)
+        })
+        .catch(function(err) {
+            res.json(err)
+        })
+})
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!")
 })
