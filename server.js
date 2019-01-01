fe66e32bde09b925f1a50b5fb912d1b2796ce54d
Will H <wandrewhouston@gmail.com>
2019-01-01 17:15:18 -0500

    combined cheerio scrapes into one loop

--- server.js
+++ server.js
@@ -5,15 +5,21 @@ var axios = require("axios")
 var db = require("./models")
 var PORT = 3000
 var app = express()
-axios.get("https://www.coolstuffinc.com/").then(function(response) {
-    var $ = cheerio.load(response.data)
-    //Check if date is in database
-    //If it is, don't add it/if not, add it
-    $("h1.display-title").each(function(i, element) {
-        var title = $(element).children().text()
-        var link = $(element).find("a").attr("href")
-    })
-    $("div.gm-article-preview-excerpt").each(function(i, element) {
-        var summary = $(element).text()
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+mongoose.connect(MONGODB_URI)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("public"))
+app.get("/scrape", function(req, res) {
+    axios.get("https://www.coolstuffinc.com/").then(function(response) {
+        var $ = cheerio.load(response.data)
+        $("div.gm-article-preview-header").each(function(i, element) {
+            var title = $(element).find("h1").text()
+            var link = $(element).find("a").attr("href")
+            var summary = $(element).find("div.gm-article-preview-excerpt").text()
+            console.log(title)
+            console.log(link)
+            console.log(summary)
+        })
     })
 })
