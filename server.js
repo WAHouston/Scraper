360c72accc852c58b51e921fe60bde2a009b3496
Will H <wandrewhouston@gmail.com>
2019-01-10 15:49:23 -0500

    fixed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose")
 var cheerio = require("cheerio")
 var axios = require("axios")
 var db = require("./models")
-var PORT = 3000
+var PORT = process.env.PORT || 3000
 var app = express()
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
 mongoose.connect(MONGODB_URI)
